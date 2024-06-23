efbb49aa9ec72f173bf32febd921369c17e51c7e
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-06-23 17:01:57 +0700

    :hammer: Implement JWT for user login and signup

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+const jwt = require("jsonwebtoken");
 const Model = require('../models/userModel');
 
 //Post Method
@@ -18,6 +19,123 @@ router.post('/', async (req, res) => {
   catch (error) {
     res.status(400).json({message: error.message})
   }
+});
+
+// Handling post request
+router.post("/signup", async (req, res, next) => {
+  const {
+    username,
+    email,
+    password
+  } = req.body;
+
+  const newUser = Model({
+    username,
+    email,
+    password,
+  });
+
+  try {
+    await newUser.save();
+  } catch {
+    const error = new Error("Error! Something went wrong.");
+    return next(error);
+  }
+  
+  let token;
+  try {
+    token = jwt.sign(
+      {
+        userId: newUser._id,
+        email: newUser.email
+      },
+      "secretkeyappearshere",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new Error("Error! Something went wrong.");
+    return next(error);
+  }
+  res.status(201).json({
+    success: true,
+    data: {
+      userId: newUser._id,
+      email: newUser.email,
+      token: token
+    },
+  });
+});
+
+// Handling post request
+router.post("/login", async (req, res, next) => {
+  let { email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await Model.findOne({ email: email });
+  } catch {
+      const error =
+        new Error(
+            "Error! Something went wrong."
+        );
+      return next(error);
+  }
+  if (!existingUser || existingUser.password != password) {
+    const error =
+      Error(
+          "Wrong details please check at once"
+      );
+    return next(error);
+  }
+  let token;
+  try {
+    //Creating jwt token
+    token = jwt.sign(
+      {
+          userId: existingUser._id,
+          email: existingUser.email
+      },
+      "secretkeyappearshere",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    console.log(err);
+    const error = new Error("Error! Something went wrong.");
+    return next(error);
+  }
+
+  res.status(200).json({
+    success: true,
+    data: {
+      userId: existingUser._id,
+      email: existingUser.email,
+      token: token,
+    },
+  });
+});
+
+router.get('/accessResource', (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  //Authorization: 'Bearer TOKEN'
+  if (!token) {
+    res.status(200).json(
+      {
+        success: false,
+        message: "Error!Token was not provided."
+      }
+    );
+  }
+  //Decoding the token
+  const decodedToken = jwt.verify(token, "secretkeyappearshere");
+  res.status(200).json(
+    {
+      success: true,
+      data: {
+        userId: decodedToken.userId,
+        email: decodedToken.email
+      }
+    }
+  );
 });
 
 //Get all Method
